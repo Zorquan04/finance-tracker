3f833904062474b4b06bbc1450c068f1ccd42458
Kacper <kacper.gumulak@gmail.com>
2026-02-14 02:24:50 +0100

    Advanced expense filtering has been added: - The ability to filter expenses by category and date range has been introduced in ExpenseViewModel and ExpenseView - Support for filter visibility, filter control commands, and the display of total expenses according to active filters have been added - The DataGrid only displays expenses that meet the filters - The interface and date selection restrictions have been improved

--- FinanceTracker/ViewModels/ExpenseViewModel.cs
+++ FinanceTracker/ViewModels/ExpenseViewModel.cs
@@ -2,6 +2,8 @@
 using FinanceTracker.Models;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace FinanceTracker.ViewModels;
 
@@ -13,36 +15,51 @@ public class ExpenseViewModel : BaseViewModel
         "Food", "Transport", "Entertainment", "Bills", "Other"
     };
 
+    public ObservableCollection<string> FilterCategories { get; } = new() 
+    { 
+        "All", "Food", "Transport", "Entertainment", "Bills", "Other" 
+    };
+
+    public ICollectionView ExpensesView { get; }
+
     private string _name = "";
     public string Name
     {
         get => _name;
-        set
-        {
-            _name = value;
-            OnPropertyChanged();
-        }
+        set { _name = value; OnPropertyChanged(); }
     }
 
     private decimal _amount;
     public decimal Amount
     {
         get => _amount;
-        set
-        {
-            _amount = value;
-            OnPropertyChanged();
-        }
+        set { _amount = value; OnPropertyChanged(); }
+    }
+
+    private decimal _totalExpenses;
+    public decimal TotalExpenses
+    {
+        get => _totalExpenses;
+        set { _totalExpenses = value; OnPropertyChanged(); }
     }
 
     private string _selectedCategory = "Other";
     public string SelectedCategory
     {
         get => _selectedCategory;
+        set { _selectedCategory = value; OnPropertyChanged(); }
+    }
+
+    private string _selectedFilterCategory = "All";
+    public string SelectedFilterCategory
+    {
+        get => _selectedFilterCategory;
         set
         {
-            _selectedCategory = value;
+            _selectedFilterCategory = value;
             OnPropertyChanged();
+            ExpensesView.Refresh();
+            UpdateTotal();
         }
     }
 
@@ -50,16 +67,51 @@ public class ExpenseViewModel : BaseViewModel
     public DateTime Date
     {
         get => _date;
+        set { _date = value; OnPropertyChanged(); }
+    }
+
+    private DateTime? _startDate = null;
+    public DateTime? StartDate
+    {
+        get => _startDate;
         set
         {
-            _date = value;
+            _startDate = value;
             OnPropertyChanged();
+            ExpensesView.Refresh();
+            UpdateTotal();
         }
     }
 
+    private DateTime? _endDate = null;
+    public DateTime? EndDate
+    {
+        get => _endDate;
+        set
+        {
+            _endDate = value;
+            OnPropertyChanged();
+            ExpensesView.Refresh();
+            UpdateTotal();
+        }
+    }
+
+    private bool _filtersVisible = false;
+    public bool FiltersVisible
+    {
+        get => _filtersVisible;
+        set { _filtersVisible = value; OnPropertyChanged(); }
+    }
+
     public ICommand AddExpenseCommand { get; }
+    public ICommand ToggleFiltersCommand { get; }
+    public ICommand ClearDateRangeCommand { get; }
+
     public ExpenseViewModel()
     {
+        ExpensesView = CollectionViewSource.GetDefaultView(Expenses);
+        ExpensesView.Filter = FilterExpenses;
+
         AddExpenseCommand = new RelayCommand(_ =>
         {
             if (string.IsNullOrWhiteSpace(Name) || Amount <= 0)
@@ -77,6 +129,34 @@ public class ExpenseViewModel : BaseViewModel
             Amount = 0;
             SelectedCategory = "Other";
             Date = DateTime.Now;
+
+            ExpensesView.Refresh();
+            UpdateTotal();
         });
+
+        ToggleFiltersCommand = new RelayCommand(_ => {FiltersVisible = !FiltersVisible; });
+        ClearDateRangeCommand = new RelayCommand(_ => {StartDate = null; EndDate = null; });
+    }
+
+    private bool FilterExpenses(object obj)
+    {
+        if (obj is not Expense expense)
+            return false;
+
+        if (SelectedFilterCategory != "All" && expense.Category != SelectedFilterCategory)
+            return false;
+
+        if (StartDate.HasValue && expense.Date < StartDate.Value)
+            return false;
+
+        if (EndDate.HasValue && expense.Date > EndDate.Value)
+            return false;
+
+        return true;
+    }
+
+    private void UpdateTotal()
+    {
+        TotalExpenses = Expenses.Where(e => FilterExpenses(e)).Sum(e => e.Amount);
     }
 }
